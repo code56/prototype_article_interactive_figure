75363a3b5d2051c49215a3615d2aea8adcd81453
code56 <evanthiak432@gmail.com>
2016-07-07 00:13:28 +0100

    form data (only name) written to txt

--- server/HTTPServer.js
+++ server/HTTPServer.js
@@ -2,17 +2,18 @@
 var http = require('http');
 var dispatcher = require ('httpdispatcher')
     ,express = require('express')
+    ,app = express()
     ,bodyParser = require('body-parser');
 var IR = require('../views/lib/interfaceregistry')
     ,interfaceregistry = new IR(); 
 var fs = require('fs'); //for writing files in node.js
 
-
-
+var multer = require('multer'); // v1.0.5
+var upload = multer();
 var encoding = 'utf8';
 //Lets define a port we want to listen to
 const PORT=8182; 
-var app = express();
+
 app.use(express.static("../public")); //put accesible public directory and its sub directories
 app.set('views', '../views'); //put accesible views
 
@@ -22,35 +23,71 @@ var hbs = require('hbs');//Handlebars
 app.set('view engine', 'html'); //we can tell Express to treat HTML files as dynamic by using the "view engine"
 app.engine('html', hbs.__express);
 
+//app.use(require('connect').bodyParser);
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
-//***Show all the components registered and let us to register a new component**/
+//
 app.get('/form', function(req, res) {
     res.render('index_node.html');
 
 });
 
-
+//**main article page**//
 app.get('/page', function(req, res) {
 
     res.render('index.html');
 });
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+//validate();
 
 // this function works - sends the name - displays it on the browser. (whatever entered in the web form) 
-app.post('/myaction', function(req, res) {
-  res.send('You sent the name "' + req.body.name + '".');
-  console.log ("the data is sent");
+
+//You cannot reference an X/HTML element until the document has loaded, or more specifically, 
+//until the element has loaded.
+//I realise that this thread is getting on now but it is an important point. 
+//By placing the Javascript at the end of the document, you are ensuring that the 
+//element has loaded before the Javascript attempts to reference it, solving the problem. 
+//Another method is to tell your Javascript to wait until the document has loaded. 
+
+app.post('/myaction', upload.array(), function(req, res, next) {
+  //res.send('You sent the name "' + req.body.name + '".');
+  validate();
+  
+  var writenData = req.body.name; // work O.K. 
+  var writeData_email = req.body.email; 
+  var form = document.querySelector("form1");
+  console.log(form.elements[1].type);
+
+  console.log('body content is' + writeData_email);
+  console.log(req.body['email']); // why does it just bring the name information and not the email infor etc. 
+  // should I put the form values in an array?
+
+  console.log('body content is' + writenData);
+  // also for writing the file, each user will be entering information in a different file. 
+  fs.writeFile('user_data.txt', writenData, function(err){
+      if(err){
+        console.log('There was an error writing the user input to file.');
+      }
+      res.send('Thank you');
+    });
+  //res.send('You sent the email "' + req.body.email + '".');
+  // accessing form elements 
+ // var name_element = res.document.getElementById(req.body.name);
+  //console.log (name_element);
 });
-
-
 
 
 //*** registry component acction send from /register***//
 // box filling
+// currently not working
 app.post('/registermetadata', function(req, res) {
   //res.send('You sent the URL "' + req.body.schema + '".');
   interfaceregistry.register(req.body.keyword, function (response){
@@ -65,7 +102,9 @@ app.get('/data', function(req, res) {
     res.render('register_metadata.html');
 });
 
+
 // to upload data opens a new HTML page the clickform.html
+// enter code to handle the selections of the drop down menu
 app.get('/clickform', function(req, res) {
     res.render('clickform.html');
 });
@@ -77,14 +116,11 @@ app.get('/web_form', function(req, res) {
     console.log('this button works');
 });
 
-// app.get('/php1', function(req, res) {
-//     res.render('enterDATA.php');
-// });
+
 
 app.get('/writeToFile1', function(res, res) {
     console.log ("this invocation writeToFile works");
-// I want to call the WriteToFile() function. 
-    //WriteToFile();
+// calling the WriteText() function. 
     WriteText();
 });
 
@@ -105,62 +141,46 @@ fs.writeFile("/Users/kaimakle/Documents/Ph.D./node_web_server/views/test", "Hey 
 }); 
 }
 
-
-
-// function to write to a file. 
-function WriteToFile()
-{
-  
-    // As stated by others, ActiveX is an IE-specific technology.
-   /* if (window.DOMParser)
-  { // Firefox, Chrome, Opera, etc.
-    parser=new DOMParser();
-    xmlDoc=parser.parseFromString(xml,"text/xml");
+// http://stackoverflow.com/questions/28372694/uncaught-referenceerror-form-is-not-defined
+function validate1(form){
+  var form = document.form,
+      name = form.name.value,
+      email = form.email.value,
+      message = form.message.value;
+  if (name.length == 0 || name.length > 200) {
+    alert ("You must enter a name.");
+    return false;
   }
-  else // Internet Explorer
-  {
-    xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
-    xmlDoc.async=false;
-    xmlDoc.loadXML(xml); 
-  } 
-*/
-// ActiveX is an IE-specific technology
 
-  var fso = new ActiveXObject("Scripting.FileSystemObject");
-  var s = fso.CreateTextFile("/Users/kaimakle/Documents/Ph.D./node_web_server/views/new.txt", true);
-  var text=document.getElementById("TextArea1").innerText;
-  s.WriteLine(text);
-  s.WriteLine('***********************');
-  s.Close();
-  console.log('we are here');
+  if (email.length == 0 || email.length > 200) {
+    alert ("You must enter a email.");
+    return false;
+  }
+
+  if (message.length == 0) {
+    alert ("You must enter a message.");
+    return false;
+  }
+
+  return true;
 }
 
 
-
-
-function createregistryMetadataHTML(){
-
-var libxslt = require('libxslt')
-    ,libxmljs = require('libxmljs')
-    ,schemaconfig = require ('../config/configschema');
-
-var xhtml = schemaconfig.SCHEMAHTML;
-var encoding = 'utf8';
-
-var docSource = fs.readFileSync(schemaconfig.SCHEMAXML, encoding);  
-var stylesheetSource = fs.readFileSync(schemaconfig.SCHEMAXSL, encoding);
-
-var stylesheet = libxslt.parse(stylesheetSource);
-var result = stylesheet.apply(docSource);
-
-//console.log(result);
-
-fs.writeFile(xhtml, result, encoding, function (err) {
-            if (err) return console.log(err);
-            else {console.log('data save into > ' + xhtml);}
-});
-
-}
+//ReferenceError: document is not defined
+//    at validate (/Users/kaimakle/Documents/Ph.D./node_web_server/server/HTTPServer.js:163:13)
+//is it a problem of order or calling the functions? 
+// http://stackoverflow.com/questions/8892631/error-document-form-is-undefined-in-javascript
+function validate() {
+    var x = document.form1["form1"]["name"].value;
+    console.log('the var x in validate function is' + x);
+    if (x == null || x == "") {
+        alert("First name cannot be left blank.");
+        return false;
+    }
+    else {
+        return true;
+    }
+} // <- here
 
 
 
