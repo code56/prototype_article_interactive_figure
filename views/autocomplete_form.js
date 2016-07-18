928c75b6e1b8df8f498b5268bca5ab2552d34c93
code56 <evanthiak432@gmail.com>
2016-07-18 16:48:59 +0100

    autocomplete

---
+++ views/autocomplete_form.js
@@ -0,0 +1,20 @@
+// the javascript with the autocomplete function
+
+/// <reference path = "jquery-1.4.4.-vsdoc.js" />
+/// <reference path = "jquery-ui.js" />
+
+// start the anonymous function once all the HTML has been loaded 
+
+$(document).ready(function(){
+
+//finding elements by the class autocomplete
+//where to go to retrieve data
+	//$(".autocomplete").autocomplete();
+
+	$(":input[data-autocomplete]").each(function() {
+		$(this).autocomplete({ source: $(this).attr("data-autocomplete") });
+
+		});
+	}
+
+})
