69d4213868b1935b0d8750f5824a641194721a43
code56 <evanthiak432@gmail.com>
2016-08-22 15:59:26 +0100

    starting tests1

--- test/test1.js
+++ test/test1.js
@@ -5,14 +5,14 @@ var sys = require("sys");
 sys.puts("Hello World");
 
 // to test the file upload of my server
-
+// http://stackoverflow.com/questions/10120866/how-to-unit-test-with-a-file-upload-in-mocha
 var should = require('should'),
     supertest = require('supertest');
 var request = supertest('localhost:8182');
 
 describe('upload', function() {
     it('a file', function(done) {
-       request.post('/uploads')
+       request.post('/uploads')  //request.post('/your/endpoint')
               .field('extra_info', '{"in":"case you want to send json along with your file"}')
               .attach('image', 'path/to/file.jpg')
               .end(function(err, res) {
@@ -21,3 +21,8 @@ describe('upload', function() {
               });
     });
 });
+
+
+
+
+
