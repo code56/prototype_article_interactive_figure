88cb75fbf38976ef7e12061be84e8b52af8b5d91
code56 <evanthiak432@gmail.com>
2017-03-21 14:32:17 +0000

    changed the testing_bash.sh to add the wget for getting data from the expvip

--- public/assets/bash/testing_bash.sh
+++ public/assets/bash/testing_bash.sh
@@ -7,5 +7,5 @@ echo "Working hard..."
 echo "changed=yes comment='something has changed' whatever=123"
 
 
-wget -O /srv/node-web-server/public/assets/bash/ “https://www.dropbox.com/sh/n15tpsqj92wfn8u/AACeafgJTWNTG2pos0EBSioOa/IWGSC1/default_metadata.txt?dl=0”
+wget -O /srv/node-web-server/public/assets/bash “https://www.dropbox.com/sh/n15tpsqj92wfn8u/AACeafgJTWNTG2pos0EBSioOa/IWGSC1/default_metadata.txt?dl=0”
 
