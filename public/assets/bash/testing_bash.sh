433aa61b32caf46431b32e4c6ae63a1a898668f3
code56 <evanthiak432@gmail.com>
2017-03-21 13:32:51 +0000

    added load_data_rake.rake

--- public/assets/bash/testing_bash.sh
+++ public/assets/bash/testing_bash.sh
@@ -6,3 +6,5 @@ echo "Working hard..."
 echo  # an empty line here so the next line will be the last.
 echo "changed=yes comment='something has changed' whatever=123"
 
+wget -O /srv/node-web-server/public/assets/bash/ "https://www.dropbox.com/sh/n15tpsqj92wfn8u/AACeafgJTWNTG2pos0EBSioOa/IWGSC1/default_metadata.txt?dl=0"
+
