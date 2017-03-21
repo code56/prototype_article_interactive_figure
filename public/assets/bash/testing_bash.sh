c6eb82accec2f720b5171e9c438b3f56e0bfe3ef
code56 <evanthiak432@gmail.com>
2017-03-21 14:48:08 +0000

    check if the default_metadata.txt exists otherwise download it

--- public/assets/bash/testing_bash.sh
+++ public/assets/bash/testing_bash.sh
@@ -6,8 +6,14 @@ echo "Working hard..."
 #echo  # an empty line here so the next line will be the last.
 echo "changed=yes comment='something has changed' whatever=123"
 
-# vim default_metadata.txt\?dl\=0 
-wget https://www.dropbox.com/sh/n15tpsqj92wfn8u/AACeafgJTWNTG2pos0EBSioOa/IWGSC1/default_metadata.txt
+
+if [ -e default_metadata.txt ]
+then 
+	echo "found default_metadata"
+else
+	wget https://www.dropbox.com/sh/n15tpsqj92wfn8u/AACeafgJTWNTG2pos0EBSioOa/IWGSC1/default_metadata.txt
+
+wget https://www.dropbox.com/sh/n15tpsqj92wfn8u/AAA7-GaMgxEgzs_dAgfwdpwca/IWGSC1/FactorOrder.tsv
 
 
 
