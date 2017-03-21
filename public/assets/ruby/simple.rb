8b8b8fc206d1c84f5eca922d52239e332a6b5815
code56 <evanthiak432@gmail.com>
2017-03-21 13:02:39 +0000

    added a stdout to be a line of name=value pairs

--- public/assets/ruby/simple.rb
+++ public/assets/ruby/simple.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby -w
 
 # this is a simple ruby file 
-echo "changed=yes comment='something has changed' whatever=123"
+puts "changed=yes comment='something has changed' whatever=123"
 
 #puts "Hello Ruby!"
 
