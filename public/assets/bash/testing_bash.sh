81fc260d38e099104749af28b27a7749e049842a
code56 <evanthiak432@gmail.com>
2017-03-21 12:12:24 +0000

    added a simple bash to see if I can run it automatically with Vagrant

---
+++ public/assets/bash/testing_bash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+echo "Working hard..."
+
+# writing the state line
+echo  # an empty line here so the next line will be the last.
+echo "changed=yes comment='something has changed' whatever=123"
+
