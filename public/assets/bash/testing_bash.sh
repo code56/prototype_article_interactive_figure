16acac68a39ae04440ea4ff5a6c54492a3dd2896
code56 <evanthiak432@gmail.com>
2017-03-21 14:42:33 +0000

    changed the testing_bash.sh to add the wget for getting data from the expvip

--- public/assets/bash/testing_bash.sh
+++ public/assets/bash/testing_bash.sh
@@ -7,5 +7,20 @@ echo "Working hard..."
 echo "changed=yes comment='something has changed' whatever=123"
 
 
-wget https://www.dropbox.com/sh/n15tpsqj92wfn8u/AACeafgJTWNTG2pos0EBSioOa/IWGSC1/default_metadata.txt?dl=0
+wget https://www.dropbox.com/sh/n15tpsqj92wfn8u/AACeafgJTWNTG2pos0EBSioOa/IWGSC1/default_metadata.txt
 
+
+
+
+
+
+# mkdir -p /srv/formulas
+# cd /srv/formulas
+# git clone https://github.com/saltstack-formulas/apache-formula.git
+
+# # or
+
+# mkdir -p /srv/formulas
+# cd /srv/formulas
+# wget https://github.com/saltstack-formulas/apache-formula/archive/master.tar.gz
+# tar xf apache-formula-master.tar.gz
