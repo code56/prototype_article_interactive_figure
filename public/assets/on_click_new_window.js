28fee32f839eb8808621a1f7f2ff682dac870538
code56 <evanthiak432@gmail.com>
2017-05-10 16:01:50 +0100

    added functionality of getting a pop up button with getting a youtube video upon pressing a button that calls the on_click_new_window.js file

---
+++ public/assets/on_click_new_window.js
@@ -0,0 +1,25 @@
+// this is the Javascript script that will be fired
+// when the button is clicked to allow a new window to pop up. 
+function displayPopUp() {
+$(document).ready(function(){
+    console.log('script is loaded');
+
+    $("#b1").click(function(){
+        var win = window.open('', '_blank', "height=400,width=450");
+        var width = 400,
+        height = 300,
+        vid='http://www.youtube.com/v/yeuCLMppZzc';
+        var obj = '<object width="' + width + '" height="' + height + '">' +
+                            '<param name="movie" value="' + vid + '" />' +
+                            '<param name="allowFullScreen" value="true" />' +
+                            '<param name="allowscriptaccess" value="always" />' +
+                            '<param name="wmode" value="opaque">' +
+                            '<embed src="' + vid + '" type="application/x-shockwave-flash" width="' + width + '" height="' + height + '" allowscriptaccess="always" allowfullscreen="true" wmode="opaque" /></object>';
+        win.document.write(obj);
+        console.log('button works');
+    });
+});
+
+};
+
+displayPopUp('#b1');
