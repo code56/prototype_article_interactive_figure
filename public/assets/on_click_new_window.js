f947435a7e73dd702beb2e4e360480ab33091530
code56 <evanthiak432@gmail.com>
2017-05-11 15:25:08 +0100

    added code that when pressing on a link the colorbox of jquery works and then brings up in a new separate window an image.

--- public/assets/on_click_new_window.js
+++ public/assets/on_click_new_window.js
@@ -2,8 +2,6 @@
 // when the button is clicked to allow a new window to pop up. 
 function displayPopUp() {
 $(document).ready(function(){
-    console.log('script is loaded');
-
     $("#b1").click(function(){
         var win = window.open('', '_blank', "height=400,width=450");
         var width = 400,
@@ -17,6 +15,8 @@ $(document).ready(function(){
                             '<embed src="' + vid + '" type="application/x-shockwave-flash" width="' + width + '" height="' + height + '" allowscriptaccess="always" allowfullscreen="true" wmode="opaque" /></object>';
         win.document.write(obj);
         console.log('button works');
+       //var open_interactive_fig = window.open('', '_blank', "height=600, width=900");
+
     });
 });
 
